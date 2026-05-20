96ece92578f2384ffae113a3b30e441ce5708b94
vchen00 <vincentchen314159@gmail.com>
2026-05-20 09:40:40 -0400

    Update autoshort.py
    comment debug print statemtnes

--- autoshort.py
+++ autoshort.py
@@ -11,8 +11,8 @@ visited = []
 
 def popnew(list, cur, rows, cols) -> bool:
     vchen.pop(0)
-    print(cur[0], end= ' ')
-    print(cur[1])
+    # print(cur[0], end= ' ')
+    # print(cur[1])
     for i in range(-1, 2):
         for j in range(-1, 2):
             if i == 0 and j == 0:
@@ -21,17 +21,17 @@ def popnew(list, cur, rows, cols) -> bool:
             c = cur[1] + j
             if r >= 0 and r < rows:
                 if c >= 0 and c < cols:
-                    print(r, end='')
-                    print(c)
+                    # print(r, end='')
+                    # print(c)
                     try:
                         if not list[r][c] == 'x' and not visited[r][c]:
                             if list[r][c] == 'e':
                                 return True
-                            print("append")
+                            # print("append")
                             vchen.append([r, c])
                             visited[r][c] = True
                     except:
-                        print("fail")
+                        # print("fail")
                         continue
     return False
 
@@ -45,13 +45,11 @@ def autoshort(list, rows, cols) -> int:
     visited = [[False for _ in range(rows)] for _ in range(cols)]
     visited[start[0]][start[1]] = True
 
-    print(list[2][2])
-
     steps = 0
     while steps < 5:
         steps += 1
         length = len(vchen)
-        print(f"len = {length}")
+        # print(f"len = {length}")
         for i in range(length):
             if popnew(list, vchen[0], rows, cols):
                 return steps
