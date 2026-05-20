3574b72a6b0f6c5b43942cbda395a523f259f192
Minr2 <choiohcen@gmail.com>
2026-05-20 09:23:40 -0400

    gridquestcommitmessage imadeagrid

--- game.py
+++ game.py
@@ -1,2 +1,35 @@
 # main game file, handles backend
-print("hi im oliver choi")
+
+array = []
+
+#setting zone - dangerous
+rows = 2
+cols = 3
+
+def grid():
+    global array, rows, cols
+    for _ in range(rows):
+        array.append([1] * cols)
+    return array
+
+def set_X(row,col):
+    array[row][col] = 0 #0th index!
+
+def set_Start(row,col):
+    array[row][col] = "S"
+
+def set_End(row,col):
+    array[row][col] = "E"
+
+def output():
+    for x in range(rows):
+        print(array[x])
+
+
+
+# testing zone...
+grid()
+set_Start(0,0)
+set_End(1,2)
+set_X(1,1)
+output()
