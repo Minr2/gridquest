4db6423da917377589c5be89e04087e2d5dfae5e
Minr2 <choiohcen@gmail.com>
2026-06-02 19:47:04 -0400

    Change title to 'GridQuest' and add a styled div

--- app.py
+++ app.py
@@ -2,4 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title("Hello world")
+st.title("GridQuest")
+
+st.markdown("""<div style=" width: 50px; height: 50px; border: 2px solid black; "> </div> """, unsafe_allow_html= True)
