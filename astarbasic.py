8ed508b8a676d28734b8fb4728c55c887d38ca58
vchen00 <vincentchen314159@gmail.com>
2026-06-03 07:24:15 -0400

    Update astarbasic.py

--- astarbasic.py
+++ astarbasic.py
@@ -53,7 +53,7 @@ def aStar(grid, start, end, rows, cols) -> list:
             ncol = cur_n.state[1] + dir[1]
             if (nrow < 0 or nrow >= rows or ncol < 0 or ncol >= cols):
                 continue
-            if grid[nrow][ncol] == 'x':
+            if grid[nrow][ncol] == 'X':
                 continue
             neighbours.append(Node(cur_n, (nrow, ncol)))
         
