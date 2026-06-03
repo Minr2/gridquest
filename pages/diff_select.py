5df0098afe6465892a7b178b15a406e93beb8cdd
Minr2 <choiohcen@gmail.com>
2026-06-02 21:48:50 -0400

    changed github file to public to fix issues possibly could be an issue with github

--- pages/diff_select.py
+++ pages/diff_select.py
@@ -5,8 +5,8 @@ st.subheader("Select Difficulty")
 
 if st.button("Beginner"):
     st.session_state.diff = "beginner"
-    st.switch_page("app")
+    st.switch_page("app.py")
 
 if st.button("Advanced"):
     st.session_state.diff = "advanced"
-    st.switch_page("app")
+    st.switch_page("app.py")
